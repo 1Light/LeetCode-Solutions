f98fa7b4e76d4ababe37580e50ad87eb0b00c80d
1Light <redclover1.618@gmail.com>
2025-03-24 17:12:03 +0300

    upload

---
+++ Backtracking/Permutations II.py
@@ -0,0 +1,34 @@
+# Permutation || (Mode: Medium)
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        result = []
+        permutation = []
+        count = {n:0 for n in nums}
+
+        for n in nums:
+            count[n] += 1
+        
+        def backtrack():
+
+            if len(permutation) == len(nums):
+                result.append(permutation[:])
+                return
+            
+            for n in count:
+                if count[n] > 0:
+                    permutation.append(n)
+                    count[n] -= 1
+
+                    backtrack()
+
+                    count[n] += 1
+                    permutation.pop()
+        
+        backtrack()
+        return result
