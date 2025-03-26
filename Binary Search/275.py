44ac1c2445a198674849107b2e92cd848d281505
1Light <redclover1.618@gmail.com>
2025-03-26 09:23:55 +0300

    upload

---
+++ Binary Search/275.py
@@ -0,0 +1,26 @@
+# 275. H-Index II
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+
+        n = len(citations)
+        left, right = 0, n - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+
+            if citations[mid] <= (n - mid):
+                left = mid + 1
+            else:
+                right = mid - 1 
+        
+        return n - left
+
+solve = Solution()
+result = solve.hIndex([1,2,100])
+print(result)
+        
