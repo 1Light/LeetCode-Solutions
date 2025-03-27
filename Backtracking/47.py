18a28d7cfbe877c24e6eee0ff2ef1e35804c403b
1Light <redclover1.618@gmail.com>
2025-03-27 13:20:50 +0300

    upload

--- Backtracking/47.py
+++ Backtracking/47.py
@@ -1,35 +1,48 @@
 # Permutation || (Mode: Medium)
 
 class Solution(object):
-    
+
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
 
+        # list to add permutations
         result = []
-        permutation = []
-        count = {n:0 for n in nums}
 
-        for n in nums:
-            count[n] += 1
-        
-        def backtrack():
+        # sort list to gather duplicates
+        nums.sort()
 
-            if len(permutation) == len(nums):
-                result.append(permutation[:])
+        # create used list to check status
+        used = [False] * len(nums)
+
+        def backtrack(path):
+
+            if len(path) == len(nums):
+                result.append(path[:])
                 return
-            
-            for n in count:
-                if count[n] > 0:
-                    permutation.append(n)
-                    count[n] -= 1
 
-                    backtrack()
+            for i in range(len(nums)):
 
-                    count[n] += 1
-                    permutation.pop()
+                if used[i]:
+                    continue
+                
+                # here, the only case where the nums[i] is not used is iteration reaches nums[i-1]
+                # is if the code initially used nums[i] as the start node and then saw every other path
+                # hence, it backtracked out of it and is now trying other elements different from nums[i]
+                # if we were to not set the code block below, we would be setting nums[i-1] as the start node
+                # since nums[i] = nums[i-1], this would cause duplication in path
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path)
+
+                path.pop()
+                used[i] = False
         
-        backtrack()
+        backtrack([])
         return result
