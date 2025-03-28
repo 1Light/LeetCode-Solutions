1182165b30a0ee92bd3a4028477a5e133ad67256
1Light <redclover1.618@gmail.com>
2025-03-28 09:56:44 +0300

    upload

--- Google/Array/1_2.py
+++ Google/Array/1_2.py
@@ -3,16 +3,18 @@
 
 def sortInWave(arr):
 
-    arr.sort()
+    for i in range(0, len(arr), 2):
+        if (i > 0 and arr[i] < arr[i-1]):
+            arr[i-1], arr[i] = arr[i], arr[i-1]
+        
+        if (i < len(arr)-1 and arr[i] < arr[i+1]):
+            arr[i], arr[i+1] = arr[i+1], arr[i]
 
-    for i in range(0, len(arr)-1, 2):
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-    
     return arr
 
 # testing
 
-arr = [10, 90, 49, 2, 1, 5, 23]
+arr=[1, 2, 3, 4, 5]
 result = sortInWave(arr)
 
 # output
